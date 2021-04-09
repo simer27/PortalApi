0a0373d3e31bf2bd7de992955d5c23696a567bbf
Simeao Ferreira de Almeida <simer27@yahoo.com>
2021-04-09 11:16:18 -0300

    teste

---
+++ smartfy.portal_api.Infra.CrossCutting.Identity/Migrations/20210408175658_PrimeiraMigracao.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace smartfy.portal_api.Infra.CrossCutting.Identity.Migrations
+{
+    public partial class PrimeiraMigracao : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
--- smartfy.portal_api.services.WebAPI/Startup.cs
+++ smartfy.portal_api.services.WebAPI/Startup.cs
@@ -125,10 +125,12 @@ namespace smartfy.portal_api.services.WebAPI
         {
             if (env.IsDevelopment())
             {
+                System.Console.WriteLine("Dev");
                 app.UseDeveloperExceptionPage();
             }
             else
             {
+                System.Console.WriteLine("Prod");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
